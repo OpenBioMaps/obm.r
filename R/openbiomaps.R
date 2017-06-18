9186d884b7e56cf907b73a3885853a6716fb4f2b
Bán Miklós <banm@vocs.unideb.hu>
2017-06-18 23:25:25 +0200

    upload data from file, attach media files

--- R/openbiomaps.R
+++ R/openbiomaps.R
@@ -59,7 +59,7 @@ OBM_init <- function (project='',url='openbiomaps.org',scope=c(),verbose=F) {
 #' @export
 #' @examples
 #' OBM_auth()
-#' token <- OBM_auth('foo@google.com','12345')
+#' token <- OBM_auth('foo@google.com','abc123')
 
 OBM_auth <- function (username='',password='',scope=OBM$scope,client_id=OBM$client_id,url=OBM$token_url,verbose=F,paranoid=F) {
     if ( exists('token', envir=OBM ,inherits=F) & exists('time', envir=OBM ,inherits=F) & (username=='' & password=='')) {
@@ -139,6 +139,12 @@ get_password <- function() {
 #' @examples
 #' data <- OBM_get('get_data','39980:39988')
 #' data <- OBM_get('get_data','faj=Parus palustris')
+#' get list of available forms
+#' data <- OBM_get('get_form_list',0)
+#' get data of a form
+#' data <- OBM_get('get_form_data',73)
+#' perform strored query 'last' is a custom label
+#' OBM_get('get_report','last')
 
 OBM_get <- function (scope='',condition='',token=OBM$token,url=OBM$pds_url,table=OBM$project) {
     if (scope=='' || condition == '') {
@@ -169,9 +175,10 @@ OBM_get <- function (scope='',condition='',token=OBM$token,url=OBM$pds_url,table
 #' Put Function
 #'
 #' @param scope currently put_data supported
-#' @param condition database column names vector, if missing default is the full list from the form
+#' @param form_header_names database column names vector, if missing default is the full list from the form
 #' @param csv_file a csv file with header row
 #' @param form_id the form's id
+#' @param api_form_data JSON array of data
 #' @param token OBM$token
 #' @param url OBM$url
 #' @param table OBM$table
@@ -180,13 +187,23 @@ OBM_get <- function (scope='',condition='',token=OBM$token,url=OBM$pds_url,table
 #'   OBM_get('get_form_list',0)
 #'   form <- OBM_get('get_form_data',57)
 #'   columns <- unlist(form[,'column'])
-#'   t <- OBM_put('put_data',columns[1:3],form_id=57,csv_file='./teszt2.csv')
+#'   t <- OBM_put('put_data',columns[1:3],form_id=57,data_file='~/teszt2.csv')
 #'
 #' using default columns list:
-#'   t <- OBM_put(scope='put_data',form_id=57,csv_file='./teszt2.csv')
+#'   t <- OBM_put(scope='put_data',form_id=57,csv_file='~/teszt2.csv')
+#'
+#' JSON upload
+#'   data  <- matrix(c(c("Tringa totanus",'egyed',"AWBO",'10'),c("Tringa flavipes",'egyed',"BYWO",'2')),ncol=4,nrow=2,byrow=T)
+#'   #colnames(data)<-c("species","nume","place","no")
+#'   t <- OBM_put(scope='put_data',form_id=57,api_form_data=as.data.frame(data),form_header_names=c('faj','szamossag','hely','egyedszam'))
 #' 
-OBM_put <- function (scope='',condition='',csv_file='',form_id='',token=OBM$token,url=OBM$pds_url,table=OBM$project) {
-    if ( scope == '' ) {
+#' with attached file
+#'   data  <- matrix(c(c("Tringa totanus",'egyed',"AWBO",'10','szamok.odt'),c("Tringa flavipes",'egyed',"BYWO",'2','a.pdf')),ncol=5,nrow=2,byrow=T)
+#'   #colnames(data)<-c("species","nume","place","no",'Attach')
+#'   t <- OBM_put(scope='put_data',form_id=57,api_form_data=as.data.frame(data),form_header_names=c('faj','szamossag','hely','egyedszam','obm_files_id'),media_file=c('~/szamok.odt','~/a.pdf'))
+#'
+OBM_put <- function (scope=NULL,form_header_names=NULL,data_file=NULL,media_file=NULL,form_id='',api_form_data='',token=OBM$token,url=OBM$pds_url,table=OBM$project) {
+    if ( is.null(scope) ) {
         return ("usage: OBM_get(scope...)")
     }
     if ( exists('token', envir=OBM, inherits=F) & exists('time', envir=OBM, inherits=F) ) {
@@ -199,19 +216,58 @@ OBM_put <- function (scope='',condition='',csv_file='',form_id='',token=OBM$toke
             OBM_refresh_token()
         }
     }
-    #t<-POST(url,body=list(access_token=OBM$token$access_token,scope='put_data',put_api_form=57,value='"a":{1,2},"b":{3,4}',table=OBM$project,file=upload_file('./teszt2.csv','text/csv')),encode="multipart")
-    #t<-POST(url,body=list(access_token=OBM$token$access_token,scope='put_data',put_api_form=57,value='["faj","hely","szamossag"]',table=OBM$project,file=upload_file('./teszt2.csv','text/csv')),encode="multipart") 
+
+    # create json from data.frame - api expect JSON array as api_form_data
+    api_form_data <- jsonlite::toJSON(api_form_data)
+
+    data_attachment <- 0
+    media_attachment <- 0
     
-    #h <- httr::POST(url,body=list(access_token=token$access_token,scope=scope,value=condition,table=table, filedata=upload_file("teszt2.csv", "text/csv")),encode="multipart")
+    if (!is.null( data_file )) {
+        data_attachment <- 1
+    } 
     
-    if (is.vector(condition)) {
-        condition <- jsonlite::toJSON(condition)
+    if (!is.null (media_file)) {
+        media_attachment <- 1
+        if ( !length(which(form_header_names=='obm_files_id')) ) {
+            print ("obm_files_id column should be exists and filled with the file names!")
+        }
     }
 
-    h <- httr::POST(url,body=list(access_token=token$access_token,scope=scope,put_api_form=form_id,value=condition,table=table, file=upload_file(csv_file, "text/csv")),encode="multipart")
+    if (!is.null(form_header_names) && is.vector(form_header_names)) {
+        form_header_names <- jsonlite::toJSON(form_header_names)
+    }
+
+
+    if (data_attachment==1) {
+        # only one attached file
+        h <- httr::POST(url,
+                    body=list(access_token=token$access_token,scope=scope,put_api_form=form_id,value=form_header_names,api_form_data=api_form_data,table=table, 
+                              file=upload_file(data_file)),
+                    encode="multipart")
+
+    } else if (media_attachment==1) {
+
+        for ( i in media_file ) {
+            h <- httr::POST(url,
+                    body=list(access_token=token$access_token, scope=scope, table=table, attached_files=upload_file(i)),
+                    encode="multipart")
+        }
+        
+        h <- httr::POST(url,
+                    body=list(access_token=token$access_token, scope=scope, table=table, put_api_form=form_id, value=form_header_names, api_form_data=api_form_data),
+                    encode="multipart")
+    
+    } else {
+        h <- httr::POST(url,
+                    body=list(access_token=token$access_token, scope=scope, table=table, put_api_form=form_id, value=form_header_names, api_form_data=api_form_data),
+                    encode="multipart")
+    }
+
     if (httr::status_code(h) != 200) {
         return(paste("http error:",httr::status_code(h) ))
     }
+
     h.list <- httr::content(h, "parsed", "application/json")
     h.list
 }
@@ -219,6 +275,7 @@ OBM_put <- function (scope='',condition='',csv_file='',form_id='',token=OBM$toke
 
 #' Set Function
 #'
+#' Experimental function!
 #' This function allows you to set rules for OBM_get.
 #' @param scope Which scope? e.g. set_join 
 #' @param condition A text condition based on column in your table
