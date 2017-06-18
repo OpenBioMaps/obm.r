5f297467eb4649dab1bb44dd29cd58521f7bd303
Bán Miklós <banm@vocs.unideb.hu>
2017-06-18 23:31:41 +0200

    OBM_ functions renamed to obm_ - it much easyer to typing... It is version 0.9 Will be 1.0 if obm_set will be finished...

--- R/openbiomaps.R
+++ R/openbiomaps.R
@@ -7,10 +7,10 @@
 #' @keywords init
 #' @export
 #' @examples
-#' OBM_init('dead_animals')
-#' OBM_init('butterflies','http://localhost/biomaps')
+#' obm_init('dead_animals')
+#' obm_init('butterflies','http://localhost/biomaps')
 
-OBM_init <- function (project='',url='openbiomaps.org',scope=c(),verbose=F) {
+obm_init <- function (project='',url='openbiomaps.org',scope=c(),verbose=F) {
         
     OBM <<- new.env()
 
@@ -52,16 +52,16 @@ OBM_init <- function (project='',url='openbiomaps.org',scope=c(),verbose=F) {
 #' @param password Your password
 #' @param scope vector OAuth2 scopes scecified in the server DEFAULTS are: get_form_data get_form_list get_profile get_data get_history
 #' @param client_id Default is R
-#' @param url OAuth2 token url OBM_init() provide it
+#' @param url OAuth2 token url obm_init() provide it
 #' @param verbose print some messages
 #' @param paranoid hide password while typing (on Linux)
 #' @keywords auth
 #' @export
 #' @examples
-#' OBM_auth()
-#' token <- OBM_auth('foo@google.com','abc123')
+#' obm_auth()
+#' token <- obm_auth('foo@google.com','abc123')
 
-OBM_auth <- function (username='',password='',scope=OBM$scope,client_id=OBM$client_id,url=OBM$token_url,verbose=F,paranoid=F) {
+obm_auth <- function (username='',password='',scope=OBM$scope,client_id=OBM$client_id,url=OBM$token_url,verbose=F,paranoid=F) {
     if ( exists('token', envir=OBM ,inherits=F) & exists('time', envir=OBM ,inherits=F) & (username=='' & password=='')) {
         # auto refresh token 
         z <- Sys.time()
@@ -73,7 +73,7 @@ OBM_auth <- function (username='',password='',scope=OBM$scope,client_id=OBM$clie
                 return(FALSE)
             }
             # expired
-            OBM_refresh_token(verbose=verbose)
+            obm_refresh_token(verbose=verbose)
         } else {
             if (verbose) {
                 print(paste("Token is valid until:",as.POSIXlt(OBM$time+OBM$token$expires_in,origin="1970-01-01")))
@@ -115,7 +115,7 @@ OBM_auth <- function (username='',password='',scope=OBM$scope,client_id=OBM$clie
 }
 
 #' unix like password function
-#' used in OBM_auth()
+#' used in obm_auth()
 #' 
 get_password <- function() {
     cat("Password: ")
@@ -131,24 +131,24 @@ get_password <- function() {
 #' This function allows you to get data from an OpenBioMaps server.
 #' @param scope Which scope? e.g. get_data 
 #' @param condition A text condition based on column in your table
-#' @param token OBM_init() provide it
-#' @param url OBM_init() provide it
+#' @param token obm_init() provide it
+#' @param url obm_init() provide it
 #' @param table optional table from project
 #' @keywords get
 #' @export
 #' @examples
-#' data <- OBM_get('get_data','39980:39988')
-#' data <- OBM_get('get_data','faj=Parus palustris')
+#' data <- obm_get('get_data','39980:39988')
+#' data <- obm_get('get_data','faj=Parus palustris')
 #' get list of available forms
-#' data <- OBM_get('get_form_list',0)
+#' data <- obm_get('get_form_list',0)
 #' get data of a form
-#' data <- OBM_get('get_form_data',73)
+#' data <- obm_get('get_form_data',73)
 #' perform strored query 'last' is a custom label
-#' OBM_get('get_report','last')
+#' obm_get('get_report','last')
 
-OBM_get <- function (scope='',condition='',token=OBM$token,url=OBM$pds_url,table=OBM$project) {
+obm_get <- function (scope='',condition='',token=OBM$token,url=OBM$pds_url,table=OBM$project) {
     if (scope=='' || condition == '') {
-        return ("usage: OBM_get(scope,condition,...)")
+        return ("usage: obm_get(scope,condition,...)")
     }
     if ( exists('token', envir=OBM, inherits=F) & exists('time', envir=OBM, inherits=F) ) {
         # auto refresh token 
@@ -157,7 +157,7 @@ OBM_get <- function (scope='',condition='',token=OBM$token,url=OBM$pds_url,table
         e <- OBM$time + OBM$token$expires_in
         if (length(e) && e < timestamp) {
             # expired
-            OBM_refresh_token()
+            obm_refresh_token()
         }
     }
     h <- httr::POST(url,body=list(access_token=token$access_token,scope=scope,value=condition,table=table),encode='form')
@@ -184,27 +184,27 @@ OBM_get <- function (scope='',condition='',token=OBM$token,url=OBM$pds_url,table
 #' @param table OBM$table
 #' @examples
 #' using own list of columns
-#'   OBM_get('get_form_list',0)
-#'   form <- OBM_get('get_form_data',57)
+#'   obm_get('get_form_list',0)
+#'   form <- obm_get('get_form_data',57)
 #'   columns <- unlist(form[,'column'])
-#'   t <- OBM_put('put_data',columns[1:3],form_id=57,data_file='~/teszt2.csv')
+#'   t <- obm_put('put_data',columns[1:3],form_id=57,data_file='~/teszt2.csv')
 #'
 #' using default columns list:
-#'   t <- OBM_put(scope='put_data',form_id=57,csv_file='~/teszt2.csv')
+#'   t <- obm_put(scope='put_data',form_id=57,csv_file='~/teszt2.csv')
 #'
 #' JSON upload
 #'   data  <- matrix(c(c("Tringa totanus",'egyed',"AWBO",'10'),c("Tringa flavipes",'egyed',"BYWO",'2')),ncol=4,nrow=2,byrow=T)
 #'   #colnames(data)<-c("species","nume","place","no")
-#'   t <- OBM_put(scope='put_data',form_id=57,api_form_data=as.data.frame(data),form_header_names=c('faj','szamossag','hely','egyedszam'))
+#'   t <- obm_put(scope='put_data',form_id=57,api_form_data=as.data.frame(data),form_header_names=c('faj','szamossag','hely','egyedszam'))
 #' 
 #' with attached file
 #'   data  <- matrix(c(c("Tringa totanus",'egyed',"AWBO",'10','szamok.odt'),c("Tringa flavipes",'egyed',"BYWO",'2','a.pdf')),ncol=5,nrow=2,byrow=T)
 #'   #colnames(data)<-c("species","nume","place","no",'Attach')
-#'   t <- OBM_put(scope='put_data',form_id=57,api_form_data=as.data.frame(data),form_header_names=c('faj','szamossag','hely','egyedszam','obm_files_id'),media_file=c('~/szamok.odt','~/a.pdf'))
+#'   t <- obm_put(scope='put_data',form_id=57,api_form_data=as.data.frame(data),form_header_names=c('faj','szamossag','hely','egyedszam','obm_files_id'),media_file=c('~/szamok.odt','~/a.pdf'))
 #'
-OBM_put <- function (scope=NULL,form_header_names=NULL,data_file=NULL,media_file=NULL,form_id='',api_form_data='',token=OBM$token,url=OBM$pds_url,table=OBM$project) {
+obm_put <- function (scope=NULL,form_header_names=NULL,data_file=NULL,media_file=NULL,form_id='',api_form_data='',token=OBM$token,url=OBM$pds_url,table=OBM$project) {
     if ( is.null(scope) ) {
-        return ("usage: OBM_get(scope...)")
+        return ("usage: obm_get(scope...)")
     }
     if ( exists('token', envir=OBM, inherits=F) & exists('time', envir=OBM, inherits=F) ) {
         # auto refresh token 
@@ -213,7 +213,7 @@ OBM_put <- function (scope=NULL,form_header_names=NULL,data_file=NULL,media_file
         e <- OBM$time + OBM$token$expires_in
         if (length(e) && e < timestamp) {
             # expired
-            OBM_refresh_token()
+            obm_refresh_token()
         }
     }
 
@@ -276,20 +276,20 @@ OBM_put <- function (scope=NULL,form_header_names=NULL,data_file=NULL,media_file
 #' Set Function
 #'
 #' Experimental function!
-#' This function allows you to set rules for OBM_get.
+#' This function allows you to set rules for obm_get.
 #' @param scope Which scope? e.g. set_join 
 #' @param condition A text condition based on column in your table
-#' @param token OBM_init() provide it
-#' @param url OBM_init() provide it
+#' @param token obm_init() provide it
+#' @param url obm_init() provide it
 #' @keywords set
 #' @export
 #' @examples
 #' automatically join tables
-#' data <- OBM_set('set_join',c('dead_animals','dead_animals_history'))
+#' data <- obm_set('set_join',c('dead_animals','dead_animals_history'))
 
-OBM_set <- function (scope='',condition='',token=OBM$token,url=OBM$pds_url) {
+obm_set <- function (scope='',condition='',token=OBM$token,url=OBM$pds_url) {
     if (scope=='' || condition == '') {
-        return ("usage: OBM_set(scope,condition,...)")
+        return ("usage: obm_set(scope,condition,...)")
     }
     if ( exists('token', envir=OBM, inherits=F) & exists('time', envir=OBM, inherits=F) ) {
         # auto refresh token 
@@ -298,7 +298,7 @@ OBM_set <- function (scope='',condition='',token=OBM$token,url=OBM$pds_url) {
         e <- OBM$time + OBM$token$expires_in
         if (length(e) && e < timestamp) {
             # expired
-            OBM_refresh_token()
+            obm_refresh_token()
         }
     }
     h <- httr::POST(url,body=list(access_token=token$access_token,scope=scope,value=condition),encode='form')
@@ -318,15 +318,15 @@ OBM_set <- function (scope='',condition='',token=OBM$token,url=OBM$pds_url) {
 #'
 #' This function allows you to refresh your OAuth2 token. It is usually a hidden function
 #' @param token
-#' url OBM_init() provide it
+#' url obm_init() provide it
 #' client_id default is R
 #' verbose
 #' @keywords refresh
 #' @export
 #' @examples
-#' OBM_refresh_token(token)
+#' obm_refresh_token(token)
 
-OBM_refresh_token <- function(token=OBM$token$refresh_token,url=OBM$token_url,client_id='R',verbose=F) {
+obm_refresh_token <- function(token=OBM$token$refresh_token,url=OBM$token_url,client_id='R',verbose=F) {
     h <- httr::POST(url,body=list(grant_type='refresh_token',refresh_token=token,client_id=client_id))
     j <- httr::content(h, "parsed", "application/json")
     if (exists('access_token',j)) {
